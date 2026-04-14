09ac709ce905cd8f8721b636b93da4541641e491
hghg919 <tgeq1023@naver.com>
2026-04-14 16:03:16 +0900

    (2026.04.14) 7주차
    몽고db ch8, ch9

--- 1주차/node.js/myContacts/app.js
+++ 1주차/node.js/myContacts/app.js
@@ -1,85 +1,24 @@
 const express = require("express");
-// const errorhandler = require("./middlewares/errorhandler");
-// const path = require("path");
+const dbConnect = require("./config/dbConnect");
 const app = express();
-// const router = express.Router();
+
 
 const port = 3000;
 
-// const requestTime = (req, res, next) => {
-//     let today = new Date();
-//     let now = today.toLocaleTimeString();
-//     req.requestTime = now;
-//     next();
-// };
+dbConnect();
 
-// app.use(requestTime);
-
-// app.get("/", (req, res) => {
-//     const responseText = `Hello Node! \n요청 시간 : ${req.requestTime}`;
-//     res.set("Content-type", "text/plain");
-//     res.send(responseText);
-// });
-
-// const logger = (req, res, next) => {
-//     console.log("User Logged");
-//     next();
-// };
+app.use(express.json());
 
 app.get("/", (req,res) => {
     res.status(200).send("Hello Node!");
 })
 
-// app.use(logger);
-
-// app.use(router);
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.use("/contacts", require("./routes/contactRoutes"));
 
-// app.get("/test", (req, res, next) => {
-//     const error = new Error("테스트용 에러");
-//     error.status = 401;
-//     next(error);
-// });
-
-// app.use(errorhandler);
 
 app.listen(port, () => {
     console.log(`${port}번 포트에서 서버 실행 중`);
 });
-
-// app.get("/", (req, res) => {
-//     // res.status(200);
-//     // res.send("Hello Node!");
-//     // const headers = req.headers;
-//     // res.send(headers);
-//     res.status(200);
-//     res.json({message: "Hello Node!"});
-// });
-
-// app.get("/contacts", (req, res) => {
-//     // res.status(200).send("Contacts Page");
-//     res.sendFile(__dirname + "/assets/contacts.html");
-// });
-
-// app.post("/contacts", (req, res) => {
-//     res.status(201).send("Create Contacts");
-// });
-
-// app.get("/contacts/:id", (req,res) => {
-//     res.status(200).send(`View contact for ID: ${req.params.id}`);
-// });
-
-// app.put("/contacts/:id", (req,res) => {
-//     res.status(200).send(`Update contact for ID: ${req.params.id}`);
-// });
-
-// app.delete("/contacts/:id", (req,res) => {
-//     res.status(200).send(`Delete contact for ID: ${req.params.id}`);
-// });
-
-// app.listen(port, () => {
-//     console.log(`${port}번 포트에서 서버 실행 중`);
-// });
--- 1주차/node.js/myContacts/routes/contactRoutes.js
+++ 1주차/node.js/myContacts/routes/contactRoutes.js
@@ -1,30 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const {getAllContacts, createContact,getContact,updateContact,deleteContact} = require("../controllers/contactController");
 
 router
 .route("/")
-.get((req, res) => {
-    res.status(200).send("Contacts Page");
-})
-.post((req, res) => {
-    console.log(req.body);
-    const {name, email, phone} = req.body;
-    if (!name || !email || !phone){
-        return res.status(400).send("필수값이 입력되지 않앗습니다.")
-    }
-    res.status(201).send("Create Contacts");
-});
+.get(getAllContacts)
+.post(createContact);
 
 router
 .route("/:id")
-.get((req, res) => {
-    res.status(200).send(`View contact for ID: ${req.params.id}`);
-})
-.put((req, res) => {
-    res.status(200).send(`Update contact for ID: ${req.params.id}`);
-})
-.delete((req, res) => {
-    res.status(200).send(`Delete contact for ID: ${req.params.id}`);
-})
+.get(getContact)
+.post(updateContact)
+.delete(deleteContact);
 
 module.exports = router;
